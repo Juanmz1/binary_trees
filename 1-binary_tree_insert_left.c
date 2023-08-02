5994d865a857284f2bce889d540c2fc4397a037d
juanmz1 <juanmzii48@gmail.com>
2023-08-02 12:48:53 -0700

    task

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,7 +9,7 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *tempNode;
+	binary_tree_t *tempNode = NULL;
 
 	tempNode = malloc(sizeof(binary_tree_t));
 	if (tempNode == NULL)
