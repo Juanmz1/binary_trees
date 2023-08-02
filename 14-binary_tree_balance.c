2583dd078b83b2effbbca77d13f2db802082c621
juanmz1 <juanmzii48@gmail.com>
2023-08-02 13:59:35 -0700

    task

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -12,9 +12,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		int left = 0, right = 0;
 
 		if (tree->right)
-			right = 1 + binary_tree_height(tree->right);
+			right += binary_tree_height(tree->right);
 		if (tree->left)
-			left = 1 + binary_tree_height(tree->left);
+			left += binary_tree_height(tree->left);
 		if (left > right)
 			return (left);
 		else
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,7 +10,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree)
 	{
 		int left = 0, right = 0;
-		
+
 		if (tree->left)
 			left = 1 + binary_tree_height(tree->left);
 		if (tree->right)
